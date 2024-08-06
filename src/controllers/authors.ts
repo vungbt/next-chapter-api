ebedc7dc2c4f1ab6ffa63fe9aaab3c330c2c006b
Bui Van Do <87635349+ehetenantayo@users.noreply.github.com>
2024-08-06 16:31:15 +0700

    chore: atuhor refactor

--- src/controllers/authors.ts
+++ src/controllers/authors.ts
@@ -1,9 +1,9 @@
 import { HttpStatus } from '@/constants'
-import { AuthorServices } from '@/services'
-import { IFindManyAuthor } from '@/types'
+import { AuthorServices } from '@/services/authors'
+import { ICreateAuthor, IFindManyAuthor } from '@/types'
 import express, { NextFunction } from 'express'
 
-const getAllAuthor = async (
+const getAllAuthors = async (
   req: express.Request<{}, {}, {}, IFindManyAuthor>,
   res: express.Response,
   next: NextFunction,
@@ -18,6 +18,67 @@ const getAllAuthor = async (
   }
 }
 
-export const AuthorControllers = {
-  getAllAuthor,
+const createAuthor = async (
+  req: express.Request<{}, {}, ICreateAuthor>,
+  res: express.Response,
+  next: NextFunction,
+) => {
+  try {
+    const { body, auth } = req
+    const result = await AuthorServices.create({ ...body })
+    return res.jsonApi(HttpStatus.OK, { ...result })
+  } catch (error) {
+    next(error)
+  }
+}
+
+const updateAuthor = async (
+  req: express.Request<{ id: string }, {}, ICreateAuthor>,
+  res: express.Response,
+  next: NextFunction,
+) => {
+  try {
+    const { id } = req.params
+    const { body, auth } = req
+    const result = await AuthorServices.update(id, { ...body })
+    return res.jsonApi(HttpStatus.OK, { ...result })
+  } catch (error) {
+    next(error)
+  }
+}
+
+const getAuthorById = async (
+  req: express.Request,
+  res: express.Response,
+  next: NextFunction,
+) => {
+  try {
+    const { id } = req.params
+    const result = await AuthorServices.getAuthorById(id)
+    return res.jsonApi(HttpStatus.OK, { ...result })
+  } catch (error) {
+    next(error)
+  }
+}
+
+const removeAuthor = async (
+  req: express.Request<{ id: string }>,
+  res: express.Response,
+  next: NextFunction,
+) => {
+  try {
+    const { id } = req.params
+    const result = await AuthorServices.remove(id)
+    return res.jsonApi(HttpStatus.OK, { ...result })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const authorControllers = {
+  getAllAuthors,
+  createAuthor,
+  getAuthorById,
+  updateAuthor,
+  removeAuthor,
 }
--- src/routers/authors.ts
+++ src/routers/authors.ts
@@ -1,9 +1,35 @@
-import { AuthorControllers } from '@/controllers'
+import { authorControllers } from '@/controllers'
+import { authenticateToken } from '@/middlewares/authMiddleware'
 import pagingMiddleware from '@/middlewares/paginationMiddleware'
-import express, { Router } from 'express'
+import { EUserRole } from '@/types'
+import { AuthorValidations } from '@/validation/author'
+import { Router } from 'express'
+import express, { NextFunction } from 'express'
 
 const router: Router = express.Router()
 
-router.get('/', pagingMiddleware, AuthorControllers.getAllAuthor)
+router.get('/', pagingMiddleware, authorControllers.getAllAuthors)
+
+router.post(
+  '/',
+  AuthorValidations.create,
+  authenticateToken([EUserRole.Admin]),
+  authorControllers.createAuthor,
+)
+
+router.get('/:id', pagingMiddleware, authorControllers.getAuthorById)
+
+router.put(
+  '/:id',
+  AuthorValidations.create,
+  authenticateToken([EUserRole.Admin]),
+  authorControllers.updateAuthor,
+)
+
+router.delete(
+  '/:id',
+  authenticateToken([EUserRole.Admin]),
+  authorControllers.removeAuthor,
+)
 
 export default router
